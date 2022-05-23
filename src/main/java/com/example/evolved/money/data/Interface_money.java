c23be2d4fd6ff1c5f77edf8accaeca7e059cee4a
Fedor Jansen <2162882@talnet.nl>
2022-05-23 13:24:02 +0200

    fedor currency

---
+++ src/main/java/com/example/evolved/money/data/Interface_money.java
@@ -0,0 +1,9 @@
+package com.example.evolved.money.data;
+
+public interface Interface_money {
+    public void money(int money);
+    public void addMoney(int money);
+    public void removeMoney(int money);
+
+    public int getMoney();
+}
